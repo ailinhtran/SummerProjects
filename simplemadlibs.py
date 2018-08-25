378a48c7cc3f1fc8df414122927e8fc74af75f62
Linh Tran <ailinh.t.6@gmail.com>
2018-08-25 12:59:59 -0600

    Number Game
    Good for kids grade 4-6

---
+++ simplemadlibs.py
@@ -0,0 +1,24 @@
+my_name = ("batman")
+
+print(my_name)
+
+print("I am " + my_name)
+
+print("Welcome to my madlibs game. Please type in a person's name!")
+
+userInput= input()
+
+print("now type in an adjective")
+
+ad=input()
+
+print("now type in a thing")
+
+name=input()
+
+print("now type in a location")
+
+location=input()
+
+print("I had a fun time with " + userInput + " he/she was really cool. Also I saw a/an" + " " + ad + " " + name + ". Today I went to " + location + ".")
+ 
